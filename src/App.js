7aa0c9044bb04a56378bba3715af57a379bc21f3
Kenzo Thomias <thomiaskenzo01@gmail.com>
2026-02-18 10:26:10 +0100

    Fix: Linter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 import RegistrationForm from './components/RegistrationForm'; 
 
 function App() {
